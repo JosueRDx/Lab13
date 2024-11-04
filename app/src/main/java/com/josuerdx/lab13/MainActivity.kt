5e07d5937c7b128d2d77e4da9fc3de07aa34bb3f
RDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-11-04 16:36:17 -0500

    feat: Integrar pantalla SizePositionAnimationScreen

--- app/src/main/java/com/josuerdx/lab13/MainActivity.kt
+++ app/src/main/java/com/josuerdx/lab13/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import com.josuerdx.lab13.animations.color.ColorChangeScreen
+import com.josuerdx.lab13.animations.size_position.SizePositionAnimationScreen
 import com.josuerdx.lab13.animations.visibility.VisibilityAnimationScreen
 import com.josuerdx.lab13.ui.theme.Lab13Theme
 
@@ -16,7 +17,8 @@ class MainActivity : ComponentActivity() {
             Lab13Theme {
                 // Llamar a las pantallas
                 // VisibilityAnimationScreen()
-                ColorChangeScreen()
+                // ColorChangeScreen()
+                SizePositionAnimationScreen()
             }
         }
     }
