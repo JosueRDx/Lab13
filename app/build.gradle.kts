bbcd0cba444b23702c5649b2d18366846f252c83
RDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-11-04 01:59:12 -0500

    chore: Añadir lifecycle-viewmodel-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Lifecycle ViewModel Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
 }
